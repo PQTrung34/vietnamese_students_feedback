e22563ffe7bd89c2a37a785e47fde729268dfedb
pqt <pqtrung34@gmail.com>
2025-08-03 16:45:08 +0700

    get and save data from huggingface

---
+++ data/data.py
@@ -0,0 +1,11 @@
+from datasets import load_dataset
+import os
+
+# Load data
+data = load_dataset("uitnlp/vietnamese_students_feedback")
+
+# Lấy thư mục hiện tại
+current_dir = os.getcwd()
+for split in data:
+    # Chuyển data sang json
+    data[split].to_json(f"{os.path.join(current_dir, 'data', split)}.json", force_ascii=False)
